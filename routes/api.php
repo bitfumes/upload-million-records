6506c13a2d6dfd3f94d2e383a43224972b0ec47a
sarthaksavvy <bitfumes@gmail.com>
2020-12-07 22:59:41 +0530

    11. upload-million-records - create API for upload

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SalesController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/upload', [SalesController::class, 'index']);
+Route::post('/upload', [SalesController::class, 'upload']);
+Route::get('/batch', [SalesController::class, 'batch']);
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-use App\Http\Controllers\SalesController;
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/upload', [SalesController::class, 'index']);
-Route::post('/upload', [SalesController::class, 'upload']);
-Route::get('/batch', [SalesController::class, 'batch']);
